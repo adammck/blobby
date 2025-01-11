7c94dced80d2362b773861b11447b26ca58b36a5
Adam Mckaig <adam.mckaig@gmail.com>
2025-01-11 15:41:17 -0500

    add locking to sstable writer

--- pkg/sstable/writer.go
+++ pkg/sstable/writer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"sort"
+	"sync"
 	"time"
 
 	"github.com/adammck/archive/pkg/types"
@@ -11,6 +12,7 @@ import (
 
 type Writer struct {
 	records []*types.Record
+	mu      sync.Mutex
 }
 
 func NewWriter() *Writer {
@@ -18,11 +20,16 @@ func NewWriter() *Writer {
 }
 
 func (w *Writer) Add(record *types.Record) error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
 	w.records = append(w.records, record)
 	return nil
 }
 
 func (w *Writer) Write(out io.Writer) (*Meta, error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
 	sort.Slice(w.records, func(i, j int) bool {
 		return w.records[i].Key < w.records[j].Key
 	})
@@ -40,7 +47,7 @@ func (w *Writer) Write(out io.Writer) (*Meta, error) {
 	for _, record := range w.records {
 		n, err := record.Write(out)
 		if err != nil {
-			return nil, fmt.Errorf("write record: %w", err)
+			return nil, fmt.Errorf("record.Write: %w", err)
 		}
 
 		m.Count++
