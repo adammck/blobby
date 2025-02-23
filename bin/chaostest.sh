dc629c5ae6a27db124a23648430f04797344965b
Adam Mckaig <adam.mckaig@gmail.com>
2025-02-23 13:48:57 -0500

    don't cache chaos test results

--- bin/chaostest.sh
+++ bin/chaostest.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 cd "$(dirname "$0")/.."
 
-go test -timeout 300s -test.v -run ^TestChaos$ github.com/adammck/blobby/pkg/blobby
+go test -timeout 300s -count 1 -test.v -run ^TestChaos$ github.com/adammck/blobby/pkg/blobby
