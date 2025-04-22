aff072d43ba6c5521360daa547bcdc62d42e201d
Adam Mckaig <adam.mckaig@gmail.com>
2025-04-21 21:35:43 -0400

    blobstore interface docs

--- pkg/api/blobstore.go
+++ pkg/api/blobstore.go
@@ -5,17 +5,24 @@ import (
 	"io"
 )
 
-// BlobStore defines low-level blob storage operations
+// BlobStore defines the interface for storing/retrieving blobs (e.g. sstables),
+// which are large (100s of MiBs) immutable files. S3 is the original/canonical
+// implementation.
 type BlobStore interface {
-	// Put stores a blob with the given key
+
+	// Put stores a blob with the given key.
 	Put(ctx context.Context, key string, data io.Reader) error
 
-	// Get retrieves a blob with the given key
-	Get(ctx context.Context, key string) (io.ReadCloser, error)
-
-	// GetRange retrieves a byte range of a blob (inclusive)
+	// GetRange retrieves a byte range of the blob with the given key. The start
+	// offset must be given, but the end may be zero to read to EOF. The caller
+	// MUST close the reader to avoid leaking resources.
 	GetRange(ctx context.Context, key string, start, end int64) (io.ReadCloser, error)
 
-	// Delete removes a blob
+	// Get retrieves the entire blob with the given key. This should be avoided
+	// in favor of GetRange where possible. The caller MUST close the reader to
+	// avoid leaking resources.
+	Get(ctx context.Context, key string) (io.ReadCloser, error)
+
+	// Delete removes the blob with the given key.
 	Delete(ctx context.Context, key string) error
 }
